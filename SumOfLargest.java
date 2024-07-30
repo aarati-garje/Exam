71ebd7ac2d79da8dbc41e4b22e03c59c39cb5f8c
aarati garje <aangarje@gmail.com>
2024-07-30 20:38:58 +0530

    Initial commit: Added Java program to find the largest number

---
+++ SumOfLargest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SumOfLargest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt user for three numbers
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+        
+        // Find the largest number
+        int largest = num1;
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+        
+        // Display the sum of the largest number (which is just the largest number itself)
+        System.out.println("The largest number is: " + largest);
+        System.out.println("The sum of the largest number is: " + largest);
+        
+        scanner.close();
+    }
+}
+
